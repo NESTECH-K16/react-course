cfece04c1080ab79bc19d692b9027fabd1e21637
Doan Quang Tinh <quangtinh.hn@gmail.com>
2023-08-01 18:09:05 +0700

    Edit and delete student

--- src/App.js
+++ src/App.js
@@ -1,191 +1,265 @@
-import { useState } from 'react'
-import './App.css'
-import Paragraph from './components/paragraph/index'
-// import Header from './components/layout/Header'
-import Header from './components/layout/Header'
+import { useState } from 'react';
+import './App.css';
 
 // Component
 
 const studentsData = [
-	// {
-	// 	id: Math.random(),
-	// 	username: 'Vương Hồng Quyết',
-	// 	phone: '362050424',
-	// },
-	// {
-	// 	id: Math.random(),
-	// 	username: 'Nguyễn Anh Tài',
-	// 	phone: '338089200',
-	// },
-	// {
-	// 	id: Math.random(),
-	// 	username: 'Phùng Văn Duy',
-	// 	phone: '911183701',
-	// },
-	// {
-	// 	id: Math.random(),
-	// 	username: 'Nguyễn Quang Huy',
-	// 	phone: '0976736018',
-	// },
-	// {
-	// 	id: Math.random(),
-	// 	username: 'Nguyễn Xuân Kỳ',
-	// 	phone: '865997380',
-	// },
-	// {
-	// 	id: Math.random(),
-	// 	username: 'Phạm Quốc Trường',
-	// 	phone: '818300154',
-	// },
-	// {
-	// 	id: Math.random(),
-	// 	username: 'Vũ Tiến Thành',
-	// 	phone: '985462373',
-	// },
-	// {
-	// 	id: Math.random(),
-	// 	username: 'Đoàn Quang Tịnh',
-	// 	phone: '336183665',
-	// },
-	// {
-	// 	id: Math.random(),
-	// 	username: 'BÙI TUẤN THẠCH',
-	// 	phone: '869647269',
-	// },
-	// {
-	// 	id: Math.random(),
-	// 	username: 'Lê Đắc Lực',
-	// 	phone: '966689312',
-	// },
-]
+  {
+    id: Math.random(),
+    username: 'Vương Hồng Quyết',
+    hobbies: 'Bong da, cau long',
+    courses: ['Java', 'PHP', 'React', 'Nodejs', 'Fullstack'],
+    phone: '0362050424',
+  },
+  // {
+  // 	id: Math.random(),
+  // 	username: 'Nguyễn Anh Tài',
+  // 	phone: '0338089200',
+  // },
+  // {
+  // 	id: Math.random(),
+  // 	username: 'Phùng Văn Duy',
+  // 	phone: '0911183701',
+  // },
+  // {
+  // 	id: Math.random(),
+  // 	username: 'Nguyễn Quang Huy',
+  // 	phone: '0976736018',
+  // },
+  // {
+  // 	id: Math.random(),
+  // 	username: 'Nguyễn Xuân Kỳ',
+  // 	phone: '0865997380',
+  // },
+  // {
+  // 	id: Math.random(),
+  // 	username: 'Phạm Quốc Trường',
+  // 	phone: '0818300154',
+  // },
+  // {
+  // 	id: Math.random(),
+  // 	username: 'Vũ Tiến Thành',
+  // 	phone: '0985462373',
+  // },
+  // {
+  // 	id: Math.random(),
+  // 	username: 'Đoàn Quang Tịnh',
+  // 	phone: '0336183665',
+  // },
+  // {
+  // 	id: Math.random(),
+  // 	username: 'BÙI TUẤN THẠCH',
+  // 	phone: '0869647269',
+  // },
+  // {
+  // 	id: Math.random(),
+  // 	username: 'Lê Đắc Lực',
+  // 	phone: '0966689312',
+  // },
+];
+const ACTION_ADD = 'ADD';
+const ACTION_EDIT = 'EDIT';
+const ACTION_DELETE = 'DELETE';
 
 const App = () => {
-	// state cua element
-	// const [state, setState] = useState(initialState)
-	const [students, setStudents] = useState(studentsData)
+  const [students, setStudents] = useState(studentsData);
+  const [studentUsername, setStudentUsername] = useState('');
+  const [phone, setPhone] = useState('');
+  const [hobbies, setHobbies] = useState('');
+  const options = ['Java', 'PHP', 'React', 'Nodejs', 'Fullstack'];
+  const [courses, setCourses] = useState([]);
 
-	const [year, setYear] = useState('2023') //[number,  callback] camel case username =
-	const [greetingClass, setGreetingClass] = useState(`Chào mừng lớp fullstack K16-2023`)
-	const [listingIntro, setListingIntro] = useState(``)
-	const [username, setUsername] = useState('')
-	const [isChecked, setIsChecked] = useState(false)
+  const [isOpenDialog, setIsOpenDialog] = useState(false);
+  const [id, setId] = useState(NaN);
+  const [action, setAction] = useState(ACTION_ADD);
 
-	const [studentUsername, setStudentUsername] = useState('')
-	const [phone, setPhone] = useState('')
+  const fillForm = ({ username, phone, hobbies, courses }) => {
+    setStudentUsername(username);
+    setPhone(phone);
+    setHobbies(hobbies);
+    setCourses(courses);
+  };
+  const clearForm = () => {
+    fillForm({ username: '', phone: '', hobbies: '', courses: [] });
+  };
 
-	// two way binding
-	const renderIntro = () => {
-		let introStr = ''
-		if (year === '2023') {
-			introStr = `Danh sách học sinh không phải là của các khoá 2022, 2021`
-		} else if (year === '2022') {
-			introStr = `Danh sách học sinh không phải là của các khoá 2023, 2021`
-		} else {
-			introStr = `Danh sách học sinh không phải là của các khoá 2023, 2022`
-		}
-		// setListingIntro(introStr)
-		return introStr
-	}
+  const handleCheckboxChange = (value) => {
+    if (courses.includes(value)) {
+      setCourses(courses.filter((val) => val !== value));
+    } else {
+      setCourses([...courses, value]);
+    }
+  };
 
-	const onChangeHandler = (event) => {
-		console.log('Change in Select...', event.target.value)
-		setYear(event.target.value) // ham bat dong bo
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (action === ACTION_ADD) {
+      // add
+      addStudentHandler();
+    } else if (action === ACTION_EDIT) {
+      // edit
+      handleSaveChange();
+    }
+  };
+  const addStudentHandler = () => {
+    const newStudent = {
+      id: Math.random(),
+      username: studentUsername,
+      phone,
+      hobbies,
+      courses,
+    };
 
-		if (event.target.value === '2023') {
-			setListingIntro(`Danh sách học sinh không phải là của các khoá 2022, 2021`)
-		} else if (event.target.value === '2022') {
-			setListingIntro(`Danh sách học sinh không phải là của các khoá 2023, 2021`)
-		} else {
-			setListingIntro(`Danh sách học sinh không phải là của các khoá 2023, 2022`)
-		}
-	}
+    const studentExisted = students.find((std) => std.phone === phone);
+    // previousState students
+    if (!studentExisted) {
+      setStudents([...students, newStudent]);
+      // alert(`Successfully!!!!`)
+    } else {
+      alert(`Phone number đã tồn tại!`);
+    }
 
-	const changeUsernameHandler = (e) => {
-		setUsername(e.target.value)
-	}
+    clearForm();
+  };
 
-	const hobbiesChangeHandler = (e) => {
-		console.log(`e.target.value`, e.target.checked)
-		setIsChecked(e.target.checked)
-	}
+  const editStudent = (id, newValue) => {
+    setStudents(
+      students.map((std) => {
+        if (std.id === id) return { id, ...newValue };
+        else return std;
+      })
+    );
+  };
+  const handleSaveChange = () => {
+    editStudent(id, {
+      username: studentUsername,
+      phone,
+      hobbies,
+      courses,
+    });
+    setId(NaN);
+    setAction(ACTION_ADD);
+    clearForm();
+  };
 
-	const addStudentHandler = (e) => {
-		e.preventDefault()
-		const newStudent = {
-			id: Math.random(),
-			username: studentUsername,
-			phone,
-		}
+  const deleteStudent = (id) => {
+    setStudents(students.filter((std) => std.id !== id));
+  };
 
-		const studentExisted = students.find((std) => std.phone === phone)
-		// previousState students
-		if (!studentExisted) {
-			setStudents([...students, newStudent])
-			// alert(`Successfully!!!!`)
-		} else {
-			// alert(`Học sinh đã tồn tại!`)
-		}
+  const handleYes = () => {
+    // Handle the "Yes" action from dialog
+    setIsOpenDialog(false);
+    deleteStudent(id);
+    setId(NaN);
+    setAction(ACTION_ADD);
+  };
 
-		setStudentUsername('')
-		setPhone('')
-	}
+  const handleNo = () => {
+    // Handle the "No" action from dialog
+    setIsOpenDialog(false);
+    setId(NaN);
+    setAction(ACTION_ADD);
+  };
 
-	return (
-		<div className='app'>
-			{/* <p className='heading'>Render UI</p>
-			<span className='year-current'>{year}</span>
-			<input className='username-input' type='text' value={username} onChange={changeUsernameHandler} /> <br />
-			<textarea name='' id='' cols='30' rows='10'></textarea>
-			<div>
-				<label htmlFor='hobby'>Code</label>
-				<input id='hobby' type='checkbox' value={isChecked} onChange={hobbiesChangeHandler} />
-			</div>
-			<select value={year} onChange={onChangeHandler}>
-				<option value='2023'>2023</option>
-				<option value='2022'>2022</option>
-				<option value='2021'>2021</option>
-			</select>
-			<p className='result'>{greetingClass}</p>
-			<p className='result'>{`Học sinh ${username} không thuộc ${renderIntro()}`}</p>
-			{username ? <p className='result'>{`${username} ${isChecked ? 'đã' : 'chưa'} có kinh nghiệm lập trình!`}</p> : ''} */}
-			<div className='student-form'>
-				<form action='' onSubmit={addStudentHandler}>
-					<div className='form-controls'>
-						<label htmlFor='username'>Username</label>
-						<input
-							id='username'
-							type='text'
-							value={studentUsername}
-							onChange={(e) => setStudentUsername(e.target.value)}
-						/>
-					</div>
-					<div className='form-controls'>
-						<label htmlFor='phone'>Phone</label>
-						<input id='phone' type='text' value={phone} onChange={(e) => setPhone(e.target.value)} />
-					</div>
-					<div className='form-actions'>
-						<button className='submit'>Add New Student</button>
-					</div>
-				</form>
-			</div>
-			<div className='students'>
-				{students.length > 0 ? (
-					students.map((student, idx) => {
-						return (
-							<div className='students-student' key={student.id}>
-								<span>{student.username}</span> <br />
-								<span>{student.phone}</span>
-								<i className='fa-solid fa-trash'></i>
-							</div>
-						)
-					})
-				) : (
-					<p className='text-center'>No student available</p>
-				)}
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className='app'>
+      <div className='student-form'>
+        <form action='' onSubmit={handleSubmit}>
+          <div className='form-controls'>
+            <label htmlFor='username'>Username</label>
+            <input
+              id='username'
+              type='text'
+              value={studentUsername}
+              onChange={(e) => setStudentUsername(e.target.value)}
+            />
+          </div>
+          <div className='form-controls'>
+            <label htmlFor='phone'>Phone</label>
+            <input
+              id='phone'
+              type='text'
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+          <div className='form-controls'>
+            <label htmlFor='hobbies'>Hobbies</label>
+            <input
+              id='hobbies'
+              type='text'
+              value={hobbies}
+              onChange={(e) => setHobbies(e.target.value)}
+            />
+          </div>
+          <div className='form-controls'>
+            <label>Courses:</label>
+            <div className='checkbox-group'>
+              {options.map((option) => (
+                <label key={option}>
+                  {option}
+                  <input
+                    type='checkbox'
+                    value={option}
+                    checked={courses.includes(option)}
+                    onChange={() => handleCheckboxChange(option)}
+                  />
+                </label>
+              ))}
+            </div>
+          </div>
+          <div className='form-actions'>
+            <button className='submit'>
+              {!isNaN(id) && action === ACTION_EDIT
+                ? 'Save change'
+                : 'Add New Student'}
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className='students'>
+        {students.length > 0 ? (
+          students.map((student, idx) => {
+            return (
+              <div className='students-student' key={student.id}>
+                <div className='students-student__content'>
+                  <span>{student.username}</span>
+                  <span>{student.phone}</span>
+                  <span>{student.hobbies}</span>
+                  <span>{student.courses.join(', ')}</span>
+                </div>
 
-export default App
+                <i
+                  className='fa-regular fa-pen-to-square'
+                  onClick={() => {
+                    setAction(ACTION_EDIT);
+                    setId(student.id);
+                    fillForm(student);
+                  }}
+                ></i>
+                <i
+                  className='fa-solid fa-trash'
+                  onClick={() => {
+                    if (!isNaN(id)) clearForm();
+                    setAction(ACTION_DELETE);
+                    setId(student.id);
+                    setIsOpenDialog(true);
+                  }}
+                ></i>
+              </div>
+            );
+          })
+        ) : (
+          <p className='text-center'>No student available</p>
+        )}
+      </div>
+      <dialog open={isOpenDialog}>
+        <p>Are you sure you want to proceed?</p>
+        <button onClick={handleYes}>Yes</button>
+        <button onClick={handleNo}>No</button>
+      </dialog>
+    </div>
+  );
+};
 
+export default App;
